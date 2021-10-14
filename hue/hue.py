359c5347b4bf6c610d9afe622c7885ddddda5b2d
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2021-10-14 19:21:36 +0900

    Make hue.Hue.get and .put

--- hue/hue.py
+++ hue/hue.py
@@ -4,7 +4,7 @@ import requests
 
 class Hue():
 
-    def __init__(self, path, id=None):
+    def __init__(self, path, id):
 
         with open(path) as f:
             df = json.load(f)
@@ -15,10 +15,14 @@ class Hue():
         self.url = f'http://{self.address}/api/{self.username}/lights'
 
     def get(self, query):  # fetch all information about the addressed resource
-        pass
+        url = f'https://{self.address}/api/{self.username}/lights/{self.id}'
+        r = requests.get(url=url).json()
+        return r
 
     def put(self, query):  # modify an addressed resource
-        requests.put(f'{self.url}/{self.light}/state', json=query)
+        url = f'https://{self.address}/api/{self.username}/lights/{self.id}/state'  # putは?でできる？
+        r = requests.put(url=f'{self.url}/{self.light}/state', json=query).json()
+        return r
 
     def post(self, query):  # create a new resource inside the addressed resource
         pass
