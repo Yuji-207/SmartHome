7165d962d430cf237d0052e9763e44ce32693dfa
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2021-10-14 19:09:48 +0900

    Change management of private information

--- hue.py
+++ hue.py
@@ -1,21 +1,17 @@
 import json
 import requests
 
-address = None  # IP address of the hue bridge  # GitHubに公開しないよう隠す
-username = None  # username
-light = {  # IDs of lights
-    'myroom1': None,
-    'myroom2': None,
-    'entrance': None
-}
-
 
 class Hue():
 
-    def __init__(self, address, username, id=None):
-        self.address = address
-        self.username = username
-        self.id = id
+    def __init__(self, path, id=None):
+
+        with open(path) as f:
+            df = json.load(f)
+
+        self.address = df['address']  # IP address of the hue bridge
+        self.username = df['username']  # username
+        self.id = id  # The ID of a light
         self.url = f'http://{self.address}/api/{self.username}/lights'
 
     def get(self, query):  # fetch all information about the addressed resource
---
+++ main.py
@@ -0,0 +1,5 @@
+light = {  # IDs of lights
+    'myroom1': None,
+    'myroom2': None,
+    'entrance': None
+}
