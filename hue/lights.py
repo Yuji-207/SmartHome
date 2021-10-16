b247a73368a1f7415e0cc4392fe9e37d953cb799
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2021-10-16 12:15:56 +0900

    Delete hue.py and move into lights.py

--- hue/lights.py
+++ hue/lights.py
@@ -1,16 +1,110 @@
-class Lights():  # 色の変換 -> colour ライブラリ
+import requests
+
+
+class Lights():
+    """
+    Light API.
+    色の変換 -> colour ライブラリ
+    """
 
     def __init__(self):
-        self.address = None
-        self.username = None
+        address = None
+        username = None
         self.id = None
-        self.url = f'https://{self.address}/api/{self.username}/lights/{self.id}/state'
-    
-    def get_all_lights(self):
-        pass
+        self.url = f'https://{address}/api/{username}/lights'
 
-    def get_new_lights(self):
-        pass
+    def get(self):
+        """
+        Gets a list of all lights that have been discovered by the bridge.
+        Gets the attributes and state of a given light.
+
+        Returns:
+            r (dict): A dict of all lights in the system.
+        """
+        if self.id is None:
+            url = self.url  # Get all lights
+        else:
+            url = self.url + f'/{self.id}'  # Get light attributes and state
+        r = requests.get(url=url)
+        r = r.json()
+        return r
+
+    def new(self, search=False):
+        """
+        Gets a list of lights that were discovered the last time a search for new lights was performed.
+        The list of new lights is always deleted when a new search is started.
+        
+
+        Starts searching for new lights.
+
+        The bridge will open the network for 40s. The overall search might take longer since the configuration
+        of (multiple) new devices can take longer. If many devices are found the command will have to be issued
+        a second time after discovery time has elapsed. If the command is received again during search the search
+        will continue for at least an additional 40s.
+        
+        When the search has finished, new lights will be available using the get new lights command. In addition,
+        the new lights will now be available by calling get all lights or by calling get group attributes on group 0.
+        Group 0 is a special group that cannot be deletedand will always contain all lights known by the bridge.
+        lights known by the bridge.
+
+
+        Args:
+            search (bool): Starts searching for new lights.
+
+        Returns:
+            r (dict): 
+        """
+        if search is None:
+            url = self.url + '/new'
+            r = requests.get(url=url)  # Get new lights
+            r = r.json()
+            return r
+        else:
+            r = requests.post(url=self.url)  # Search for new lights
+            r = r.json()
+            return r
+
+    def set(self, on=None, bri=None, hue=None, sat=None, xy=None, ct=None, alert=None, effect=None, transitiontime=None):
+        if self.id is not None:
+            if on is not None:
+                self.query['on'] = on
+                state = True
+            if bri is not None:
+                self.query['bri'] = bri
+                state = True
+            if hue is not None:
+                self.query['hue'] = hue
+                state = True
+            if sat is not None:
+                self.query['sat'] = sat
+                state = True
+            if xy is not None:
+                self.query['xy'] = xy
+                state = True
+            if ct is not None:
+                self.query['ct'] = ct
+                state = True
+            if alert is not None:
+                self.query['alert'] = alert
+                state = True
+            if effect is not None:
+                self.query['effect'] = effect
+                state = True
+            if transitiontime is not None:
+                self.query['transitiontime'] = transitiontime
+                state = True
+            if state is None:
+                url = self.url + f'{self.id}'
+                r = requests.pur(url=url)  # Set light attributes (rename)
+                r = r.json()
+                return r
+            else:
+                url = self.url + f'{self.id}/state'
+                r = requests.put(url=url, json=self.query)  # Set light state
+                r = r.json()
+                return r
+        else:
+            print('You NEED the ID!')
 
     def on(self):  # Turn on
         self.query['on'] =  True
